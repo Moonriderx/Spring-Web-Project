414330551976d2680a9b764986064ef5ec017fca
Moonriderx <Luben1993@abv.bg>
2021-10-12 12:29:59 +0300

    Initial Commit
    Initial Project Commit

---
+++ SpringApp/src/test/java/com/moonrider/SpringApp/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonrider.SpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
